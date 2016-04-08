f4408d1a29890a2baa3a233fe2fcec4d337a82cc
Alec S <linuxbash8@gmail.com>
2016-04-08 16:38:25 -0500

    add main code

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ impl State {
         let res = self.stack.pop();
         if std::option::Option::is_none(&res) {
             std::io::stderr()
-               .write("Stack is empty!"
+               .write("Stack is empty!\n"
                .as_bytes())
                .unwrap();
                std::process::exit(1);
@@ -67,14 +67,24 @@ impl State {
         top == 0
     }
 
-    fn is_gt(&mut self) -> bool {
+    fn is_gt(&mut self) {
         let top = self.pop();
-        top > 0
+        if top > 0 {
+            self.push(1);
+        }
+        else {
+            self.push(0);
+        }
     }
 
-    fn is_lt(&mut self) -> bool {
+    fn is_lt(&mut self) {
         let top = self.pop();
-        top < 0
+        if top < 0 {
+            self.push(1);
+        }
+        else {
+            self.push(0);
+        }
     }
 
     fn negate(&mut self) {
@@ -110,8 +120,13 @@ impl State {
         }
     }
 
-    fn is_empty(&self) -> bool {
-        self.stack.is_empty()
+    fn is_empty(&mut self) {
+        if self.stack.is_empty() {
+            self.push(1);
+        }
+        else {
+            self.push(0);
+        }
     }
 }
 
@@ -134,24 +149,37 @@ fn lines(v : Vec<u8>) -> Vec<(Vec<char>, i32)> {
 }
 
 fn print_char(val : i32) {
-    let mut buf = [val as u8; 1];
-    std::io::stdout.write(&buf).unwrap();
+    let buf = [val as u8; 1];
+    std::io::stdout().write(&buf).unwrap();
 }
 
 fn read_char() -> i32 {
-    0
+    let mut buf = [0 as u8; 1];
+    std::io::stdin().read(&mut buf).unwrap();
+    return buf[0] as i32;
 }
 
 
 fn main() {
-    let mut f = File::open("tempy.txt").unwrap();
+    let fname_option = std::env::args().nth(1);
+    if fname_option.is_none() {
+        std::io::stderr()
+           .write("Please Provide a file name!\n"
+           .as_bytes())
+           .unwrap();
+           std::process::exit(1);
+    }
+    let fname = fname_option.unwrap();
+    let mut f = File::open(fname).unwrap();
     let mut read_in = Vec::new();
     f.read_to_end(&mut read_in).unwrap();
     let lines_in = lines(read_in);
+    let numrows = lines_in.iter().count() as i32;
     let mut st = State { xpos: 0, ypos: 0, dir: Direction::Right, stack: Vec::new()};
 
-    while true {
+    loop {
         let target_char = lines_in[st.ypos as usize].0[st.xpos as usize];
+        //println!("Executing character: {}", target_char);
         match target_char {
             '>' => {
                 st.dir = Direction::Right;
@@ -160,10 +188,10 @@ fn main() {
                 st.dir = Direction::Left;
             },
             '^' => {
-                st.dir = Direction.Up;
+                st.dir = Direction::Up;
             },
             'v' => {
-                st.dir = Direction.Down;
+                st.dir = Direction::Down;
             },
             '0' => st.push(0),
             '1' => st.push(1),
@@ -175,20 +203,51 @@ fn main() {
             '7' => st.push(7),
             '8' => st.push(8),
             '9' => st.push(9),
+            'p' => { st.pop(); },
             '@' => st.dup(),
             '+' => st.add(),
             '-' => st.sub(),
             '*' => st.mult(),
             '/' => st.div(),
-            ';' => std::process:exit(0),
-
+            ';' => std::process::exit(0),
+            '|' => {
+                if st.is_zero() {
+                    st.dir = Direction::Up;
+                }
+                else {
+                    st.dir = Direction::Down;
+                }
+            },
+            '_' => {
+                if st.is_zero() {
+                    st.dir = Direction::Left;
+                }
+                else {
+                    st.dir = Direction::Right;
+                }
+            },
+            'e' => st.is_empty(),
+            '!' => st.logical_neg(),
+            '~' => st.negate(),
+            '.' => print_char(st.pop()),
+            ',' => st.push(read_char()),
+            'g' => st.is_gt(),
+            'l' => st.is_lt(),
+            ' ' => (),
              _  => {
                  std::io::stderr()
-                    .write(format!("Unknown Character in program: '{}' ", target_char)
+                    .write(format!("Unknown Character in program: '{}' \n", target_char)
                     .as_bytes())
                     .unwrap();
                     std::process::exit(1);
                 },
+        }
+        let (new_y, new_x) = st.next_step();
+        if new_y < 0 || new_y >= numrows {
+            std::process::exit(0);
+        }
+        if new_x < 0 || new_x >= lines_in[new_y as usize].1 {
+            std::process::exit(0);
         }
     }
 }
